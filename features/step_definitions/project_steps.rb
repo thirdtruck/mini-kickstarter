87ef57d59aa0e0e800685d760185c0f7b127445c
J. C. Holder <github.com@thirdtruck.org>
2016-08-17 19:56:48 -0400

    Draft project step definitions.

---
+++ features/step_definitions/project_steps.rb
@@ -0,0 +1,27 @@
+Given(/^a project with a valid name$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^a valid target dollar amount$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^a project called "([^"]*)"$/) do |project_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^a target dollar amount of "([^"]*)"$/) do |target_dollar_amount|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the "([^"]*)" command is invoked$/) do |command|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^Mini Kickstarter should accept the command input$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^Mini Kickstarter should reject the command input$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
