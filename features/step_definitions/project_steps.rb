b8734d4be4b509263e37a97f2253d8aabe0c15cd
J. C. Holder <github.com@thirdtruck.org>
2016-08-17 19:58:54 -0400

    Assign test parameters.

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -1,20 +1,20 @@
 Given(/^a project with a valid name$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  @project_name = "example"
 end
 
 Given(/^a valid target dollar amount$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  @target_dollar_amount = "1000000.00"
 end
 
 Given(/^a project called "([^"]*)"$/) do |project_name|
-  pending # Write code here that turns the phrase above into concrete actions
+  @project_name = project_name
 end
 
 Given(/^a target dollar amount of "([^"]*)"$/) do |target_dollar_amount|
-  pending # Write code here that turns the phrase above into concrete actions
+  @target_dollar_amount = target_dollar_amount
 end
 
-When(/^the "([^"]*)" command is invoked$/) do |command|
+When(/^the "([^"]*)" command is invoked$/) do |command_name|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
