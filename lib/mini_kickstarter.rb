93cc130401884cf50756c7aa8851e2507334eb91
J. C. Holder <github.com@thirdtruck.org>
2016-08-18 18:45:13 -0400

    Extract #invoke_project and #invoke_command from MiniKickstarter#invoke.

--- lib/mini_kickstarter.rb
+++ lib/mini_kickstarter.rb
@@ -4,17 +4,30 @@ class MiniKickstarter
   JUST_DOLLARS_AND_CENTS = /\A([1-9][0-9]*|[0-9])\.[0-9]{2}\z/
 
   def invoke(command_name, command_params)
+    # TODO: Compile all errors before reporting instead of reporting only the first match. Better UX.
+    case command_name
+    when "project"
+      invoke_project(command_params)
+    when "back"
+      invoke_back(command_params)
+    else
+      "ERROR: Unrecognized command."
+    end
+  end
+
+  private
+
+  def invoke_project(command_params)
     project_name = command_params[:project_name]
     target_dollar_amount = command_params[:target_dollar_amount]
 
     # We could also capture the length requirements in the regular expression, but informative
     # error messages take priority.
-    #
+
     # Separating the amount requirements for a better error UX, too.
-    #
-    # TODO: Compile all errors before reporting instead of reporting only the first match. Better UX.
+
     # TODO: Do we need to use #mb_chars here? Doesn't seem to be the case.
-    if project_name !~ ALPHANUMERIC_WITH_UNDERSCORES_DASHES 
+    if project_name !~ ALPHANUMERIC_WITH_UNDERSCORES_DASHES
       "ERROR: Projects should be alphanumeric. Undersores or dashes are allowed."
     elsif project_name.length < 4 || project_name.length > 20
       "ERROR: Projects should be no shorter than 3 characters but no longer than 20 characters."
@@ -25,5 +38,9 @@ class MiniKickstarter
     else
       "Success"
     end
+  end
+
+  def invoke_back(command_params)
+    "ERROR: Unimplemented command."
   end
 end
