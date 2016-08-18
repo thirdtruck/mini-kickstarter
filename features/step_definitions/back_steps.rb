1a2d63fcb6c0b93e7e88df540728d2980176dc89
J. C. Holder <github.com@thirdtruck.org>
2016-08-18 18:28:19 -0400

    Draft pending "backer" command step definitions.

---
+++ features/step_definitions/back_steps.rb
@@ -0,0 +1,23 @@
+Given(/^a valid given name$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^a valid credit card number$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^a valid backing amount$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^a given name of "([^"]*)"$/) do |given_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^a credit card number of "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^a backing amount of "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
