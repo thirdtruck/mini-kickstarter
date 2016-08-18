00204a3a22d47b7c5408d01c525098216a4804ec
J. C. Holder <github.com@thirdtruck.org>
2016-08-18 19:32:06 -0400

    Implement Luhn-10 validation for credit card numbers.

--- lib/mini_kickstarter.rb
+++ lib/mini_kickstarter.rb
@@ -56,6 +56,8 @@ class MiniKickstarter
       "ERROR: Projects should be no shorter than 4 characters but no longer than 20 characters."
     elsif credit_card_number.length > 19
       "ERROR: Credit card number should be no more than 19 characters."
+    elsif ! valid_luhn_10_sequence?(credit_card_number.split(//).map(&:to_i))
+      "ERROR: Invalid credit card number." # TODO: What's a better error message?
     elsif backing_amount =~ /\$/
       "ERROR: Target dollar amount should not use the $ currency symbol."
     elsif backing_amount !~ JUST_DOLLARS_AND_CENTS
@@ -63,5 +65,24 @@ class MiniKickstarter
     else
       "Success"
     end
+  end
+
+  def valid_luhn_10_sequence?(digits)
+    odd_digits = []
+    even_digits = []
+
+    # TODO: Investigate more concise options.
+    digits.reverse.each_index do |index|
+      if (index+1) % 2 == 0
+        even_digits << digits[index]
+      else
+        odd_digits << digits[index]
+      end
+    end
+
+    total = odd_digits.reduce(&:+)
+    even_digits.each { |ed| total += (2*ed).to_s.split(//).map(&:to_i).reduce(&:+) }
+
+    (total % 10) == 0
   end
 end
