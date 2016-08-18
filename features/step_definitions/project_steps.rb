09a252bf6ef608b4059b0888eb3cb5e29af25a83
J. C. Holder <github.com@thirdtruck.org>
2016-08-18 18:49:28 -0400

    Make "command is invoked" step definition command-specific. Move "Mini Kickstarter should accept/reject the command input" step definitions into shared_steps.rb.

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -14,17 +14,9 @@ Given(/^a target dollar amount of "([^"]*)"$/) do |target_dollar_amount|
   @target_dollar_amount = target_dollar_amount
 end
 
-When(/^the "([^"]*)" command is invoked$/) do |command_name|
+When(/^the "project" command is invoked$/) do
   mini_kickstarter = MiniKickstarter.new
-  @command_response = mini_kickstarter.invoke(command_name,
+  @command_response = mini_kickstarter.invoke("project",
                                               project_name: @project_name,
                                               target_dollar_amount: @target_dollar_amount)
-end
-
-Then(/^Mini Kickstarter should accept the command input$/) do
-  expect(@command_response).to eq('Success')
-end
-
-Then(/^Mini Kickstarter should reject the command input$/) do
-  expect(@command_response).to match(/^ERROR:/)
 end
---
+++ features/step_definitions/shared_steps.rb
@@ -0,0 +1,7 @@
+Then(/^Mini Kickstarter should accept the command input$/) do
+  expect(@command_response).to eq('Success')
+end
+
+Then(/^Mini Kickstarter should reject the command input$/) do
+  expect(@command_response).to match(/^ERROR:/)
+end
