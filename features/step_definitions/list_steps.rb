4d45b1b4fce88e95c694c44adc53b2da84f03280
J. C. Holder <github.com@thirdtruck.org>
2016-08-20 22:42:48 -0400

    Update "list" command step definitions to expect errors.

--- features/step_definitions/list_steps.rb
+++ features/step_definitions/list_steps.rb
@@ -1,6 +1,10 @@
 When(/^the "list" command is invoked$/) do
   mini_kickstarter = MiniKickstarter.new
-  @command_response = mini_kickstarter.invoke(@db, "list", {})
+  begin
+    @command_response = mini_kickstarter.invoke(@db, "list", {})
+  rescue MiniKickstarter::InvalidCommandParameterError => e
+    @command_response = "ERROR: #{e.message}"
+  end
 end
 
 Then(/^Mini Kickstarter should respond with:$/) do |expected_response|
