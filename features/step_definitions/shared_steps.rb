676e06ad32752230258a58b3acc64eac5c25d088
J. C. Holder <github.com@thirdtruck.org>
2016-08-21 17:32:49 -0400

    Test for explicit response messages instead of for just success/reject responses.

--- features/step_definitions/shared_steps.rb
+++ features/step_definitions/shared_steps.rb
@@ -2,12 +2,6 @@ Then(/^Mini Kickstarter should accept the command input$/) do
   expect(@command_response).to eq('Success')
 end
 
-Then(/^Mini Kickstarter should reject the command input$/) do
-  puts @command_response
-  expect(@command_response).to match(/^ERROR:/)
-end
-
-Then(/^Mini Kickstarter should respond to the command input with the message "([^"]*)"$/) do |message|
-  puts @command_response
+Then(/^Mini Kickstarter should respond with "([^"]*)"$/) do |message|
   expect(@command_response).to eq(message)
 end
