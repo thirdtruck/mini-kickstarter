17128d6078a971fb7cec0863d27408417d6b8527
J. C. Holder <github.com@thirdtruck.org>
2016-08-25 19:19:29 -0400

    Simplify argument indexing in anticipation of counting.

--- lib/mini_kickstarter_cli.rb
+++ lib/mini_kickstarter_cli.rb
@@ -2,29 +2,29 @@ require_relative 'mini_kickstarter'
 
 class MiniKickstarterCLI
   def self.parse_and_invoke(db, arguments)
-    command_name = arguments[0]
+    command_name = arguments.shift # Shifting to simplify counting/indexing below
 
     command_params = case command_name
 
     when "project"
       {
-        project_name: arguments[1],
-        target_dollar_amount: arguments[2]
+        project_name: arguments[0],
+        target_dollar_amount: arguments[1]
       }
     when "back"
       {
-        given_name: arguments[1],
-        project_name: arguments[2],
-        credit_card_number: arguments[3],
-        backing_amount: arguments[4]
+        given_name: arguments[0],
+        project_name: arguments[1],
+        credit_card_number: arguments[2],
+        backing_amount: arguments[3]
       }
     when "list"
       {
-        project_name: arguments[1]
+        project_name: arguments[0]
       }
     when "backer"
       {
-        given_name: arguments[1],
+        given_name: arguments[0],
       }
     end
 
