68b164e6e7f15dd3ef096c655a69c8ab9fafdd35
J. C. Holder <github.com@thirdtruck.org>
2016-08-24 20:09:39 -0400

    Invoke "list" command properly in scenarios. Create DB in memory as needed for "list" scenarios.

--- features/step_definitions/list_steps.rb
+++ features/step_definitions/list_steps.rb
@@ -1,7 +1,9 @@
 When(/^the "list" command is invoked$/) do
+  @db ||= MiniKickstarterDB.new(':memory:')
+
   mini_kickstarter = MiniKickstarter.new
   begin
-    @command_response = mini_kickstarter.invoke(@db, "list", {})
+    @command_response = mini_kickstarter.invoke(@db, "list", project_name: @project_name)
   rescue MiniKickstarter::InvalidCommandParameterError => e
     @command_response = "ERROR: #{e.message}"
   end
